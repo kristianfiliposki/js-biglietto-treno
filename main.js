64fd3167ec8cd861010fd7e8a48818797d9bba5e
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-10-26 18:03:42 +0200

    1.1

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ const nkm=parseInt(prompt("inserisci i km che vuoi percorrere."));
 const eta=parseInt(prompt("inserisci la tua eta."));
 const price= nkm * 0.21
 
-if(eta < 18){
-    document.getElementById("prezzo").innerHTML= (price  * 0.2) ;
+if(eta <= 18){
+    document.getElementById("prezzo").innerHTML= (price - (price  * 0.2)) ;
 } 
-else if (eta > 18) {
-    document.getElementById("prezzo").innerHTML=  (price  * 0.4) ;
+else if (eta >= 65) {
+    document.getElementById("prezzo").innerHTML= (price - (price  * 0.4)) ;
 }
+
+else{
+    document.getElementById("prezzo").innerHTML= (price) ;
+}
+
