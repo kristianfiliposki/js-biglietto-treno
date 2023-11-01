5712c2fe71e7490f46fda80a114823f020de8125
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-01 19:09:12 +0100

    1.0

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-const nkm=parseInt(prompt("inserisci i km che vuoi percorrere."));
+let nameInput = document.querySelector("#name");
+let kmInput = document.querySelector("#kilometres");
+let ageInput = document.querySelector("#age");
+let btn = document.querySelector("#generates");
+
+btn.addEventListener("click", function() {
+    console.log("L'utente ha scritto: " + nameInput.value);
+    console.log("L'utente ha scritto: " + kmInput.value);
+    console.log("L'utente ha scritto: " + ageInput.value);
+});
+
+
+
+
+
+/* const nkm=parseInt(prompt("inserisci i km che vuoi percorrere."));
 const eta=parseInt(prompt("inserisci la tua eta."));
-const price= nkm * 0.21
+const price= nkm * pkm
+const pkm=0.21
+
+document.getElementById("prezzo").innerHTML= (price) ;
+console.log(price)
 
 if(eta <= 18){
     document.getElementById("prezzo").innerHTML= (price - (price  * 0.2)) ;
@@ -9,7 +28,6 @@ else if (eta >= 65) {
     document.getElementById("prezzo").innerHTML= (price - (price  * 0.4)) ;
 }
 
-else{
-    document.getElementById("prezzo").innerHTML= (price) ;
-}
 
+
+ */
